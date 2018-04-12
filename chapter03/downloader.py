36a2579939368b6397aba438c0137aa7aec02e4b
robert.bu <buxiaoliang2008@163.com>
2018-04-11 22:41:31 -0700

    [Update]

--- chapter03/downloader.py
+++ chapter03/downloader.py
@@ -43,6 +43,29 @@ class Downloader:
                 self.cache[url] = result
         return result['html']
 
+    def download(self, url, headers, proxy, num_retries, data=None):
+        print 'Downloading:', url
+        request = urllib2.Request(url, data, headers or {})
+        opener = self.opener or urllib2.build_opener()
+        if proxy:
+            proxy_params = {urlparse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib2.ProxyHandler(proxy_params))
+        try:
+            response = opener.open(request)
+            html = response.read()
+            code = response.code
+        except Exception as e:
+            print 'Download error:', str(e)
+            html = ''
+            if hasattr(e, 'code'):
+                code = e.code
+                if  num_retries > 0 and 500 <= code < 600:
+                    # retry 5XX HTTP errors
+                    return self._get(url, headers, proxy, num_retries - 1, data)
+            else:
+                code = None
+        return {'html': html, 'code': code}
+
 class Throttle:
     """Throttle downloading by sleeping between requests to same domain
     """
@@ -50,12 +73,12 @@ class Throttle:
         # amount of delay between downloads for each domain
         self.delay = delay
         # timestamp of when a domain was last accessed
-        self.domain = {}
+        self.domains = {}
 
     def wait(self, url):
         """Delay if have accessed this domain recently
         """
-        domain = urlparse.urlsplit(url.netloc)
+        domain = urlparse.urlsplit(url).netloc
         last_accessed = self.domains.get(domain)
         if self.delay > 0 and last_accessed is not None:
             sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
